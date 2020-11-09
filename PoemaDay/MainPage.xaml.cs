c831f91473e75244bf99bc60d1465135d420b8df
yossaj <yossaj@icloud.com>
2020-11-09 20:03:16 +0000

    poem now shown when app loads

--- PoemaDay/MainPage.xaml.cs
+++ PoemaDay/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using PoemaDay.model;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
@@ -17,17 +18,35 @@ namespace PoemaDay
         public MainPage()
         {
             InitializeComponent();
+           
         }
 
 
-        void GetPoemAsync()
+        async void GetPoemAsync()
         {
-            var request = WebRequest.Create("https://poetrydb.org/linecount/6") as HttpWebRequest;
-            request.Method = "GET";
-            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
-            var info = httpResponse.StatusCode;
+            List<Poem> poems = new List<Poem>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+               var response = await httpClient.GetAsync("https://poetrydb.org/linecount/6");
+                var json = await response.Content.ReadAsStringAsync();
+                var poemList = JsonConvert.DeserializeObject<List<Poem>>(json);
+                var leng = poemList.Count();
+                PoemTitle.Text = poemList[0].title;
+                PoemAuthor.Text = poemList[0].author;
+                var lines = new List<Line>();
+                foreach(string pline in poemList[0].lines)
+                {
+                    Line newLine = new Line()
+                    {
+                        PoemLine = pline
+                    };
+                    lines.Add(newLine);
+                }
+                poemLineListView.ItemsSource = lines;
 
-           
+
+            }
+
         }
 
 
--- PoemaDay/model/Poem.cs
+++ PoemaDay/model/Poem.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PoemaDay.model
 {
     public class Poem
     {
 
-        public int id { get; set; }
-
         public string title { get; set; }
 
         public string author { get; set; }
 
+        [JsonProperty("lines")]
         public List<string> lines { get; set; }
 
         public string linecount { get; set; }
     }
+
+    public class Line
+    {
+        public string PoemLine { get; set; }
+    }
+
+
 }
