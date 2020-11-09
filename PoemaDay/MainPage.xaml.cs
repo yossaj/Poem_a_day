fb570bb5a245e4f11a8035f7a87d5da4ed21d3b4
yossaj <yossaj@icloud.com>
2020-11-09 12:38:56 +0000

    added model

--- PoemaDay/MainPage.xaml.cs
+++ PoemaDay/MainPage.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using PoemaDay.model;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace PoemaDay
 {
@@ -14,5 +18,26 @@ namespace PoemaDay
         {
             InitializeComponent();
         }
+
+
+        void GetPoemAsync()
+        {
+            var request = WebRequest.Create("https://poetrydb.org/linecount/6") as HttpWebRequest;
+            request.Method = "GET";
+            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+            var info = httpResponse.StatusCode;
+
+           
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            GetPoemAsync();
+        }
     }
+
+   
 }
---
+++ PoemaDay/model/Poem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoemaDay.model
+{
+    public class Poem
+    {
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public List<string> lines { get; set; }
+
+        public string linecount { get; set; }
+    }
+}
